10435dc3295a2baafc8de242c34f714b02966014
Andrey Novikov <NovikovAA.it@relef.ru>
2021-01-09 21:57:50 +0300

    add select

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -36,6 +36,45 @@ export class CarComponent implements OnInit {
     }
   }
 
+  carSelect = (name: string) => {
+    if (name === 'bmw') {
+      this.name = 'BMW'
+      this.speed = 240,
+      this.model = 'X5',
+      this.colors = {
+        car: 'Silver',
+        interior: 'Black',
+        wheels: 'White'
+      }
+      this.options = ["GPS", "круиз-контроль"]
+    }
+
+    if (name === 'audi') {
+      this.model = 'Audi'
+      this.speed = 250,
+      this.model = 'RS Q8',
+      this.colors = {
+        car: 'White',
+        interior: 'Black',
+        wheels: 'Silver'
+      }
+      this.options = ["ABS", "Автопилот", "Авто Паркинг"]
+    }
+
+    if (name === 'mercedes') {
+      this.name = 'Mercedes'
+      this.speed = 180,
+      this.model = 'C180',
+      this.colors = {
+        car: 'Silver',
+        interior: 'Black',
+        wheels: 'White'
+      }
+      this.options = ["Просмотр фильмов"]
+    }
+    
+  }
+
 }
 
 interface Colors {
